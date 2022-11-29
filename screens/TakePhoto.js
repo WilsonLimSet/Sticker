7fcaf015ab8ea3ba08a7316901b35304e42acc93
Sarah Liang <seliang@usc.edu>
2022-11-29 00:27:45 -0800

    takePhoto screen structure

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { React, useState, useCallback } from 'react'
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import * as Clipboard from 'expo-clipboard';
 import colors from '../colors';
@@ -10,8 +10,13 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 export default function TakePhoto() {
   return (
     <View style={styles.container}>
-
-      <Text style={styles.title}>Take Photo</Text>     
+        <View style={styles.imgContainer}>
+            <Image style={styles.img} source={require('../assets/takePhoto.jpeg')} />
+        </View>
+        <View style={styles.cameraBar}>
+            {/* ICONS HERE */}
+        </View>
+        {/* going to need to figure out how to remove the tab bar at the bottom via App.js */}
     </View>
 );
 }
@@ -22,153 +27,17 @@ const styles = StyleSheet.create({
 container: {
     flex: 1,
     backgroundColor: colors.darkGray,
-    // paddingLeft: 18
-},
-statsSection: {
-    marginTop: "5%",
-    paddingLeft: 18
-},
-statSection: {
-    display: "flex",
-    flexDirection: "row",
-    paddingLeft: 5,
-    marginBottom: "1%",
-},
-sectionIcon: {
-    marginRight: "3%",
-},
-title: {
-    fontSize: 21,
-    fontWeight: "600",
-    color: "white",
-    marginBottom: "3%",
-},
-text: {
-    color: "white",
-    fontSize: 15,
-    marginBottom: 3,
-},
-leaderboard: {
-    position: "relative",
-    paddingLeft: 18
-},
-leaderboardSectionHeader: {
-    backgroundColor: "#70A1B7",
-    width: 114,
-    height: 32,
-    borderRadius: 5,
-    position: "absolute",
-    top: 15,
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: 30,
-},
-leaderboardHeaderText: {
-    fontSize: 16,
-    fontWeight: "400",
-    color: "white"
-},
-leaderboardSection: {
-    backgroundColor: "#616f74",
-    width: 335,
-    height: 177,
-    borderRadius: 10,
-    zIndex: -5,
-    marginTop: 30,
-    justifyContent: "center",
-    paddingLeft: 15,
-},
-leaderboardSectionContainer: {
     
 },
-leaderboardFirstPerson: {
-    flexDirection: "row",
-    alignItems: "center",
+imgContainer: {
+    marginTop: 25,
+    height: 566,
+    width: 394,
+}, 
+img: {
+    borderRadius: 20,
 },
-leaderboardPerson: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 12,
-},
-leaderboardPersonMetric: {
-    flexDirection: "row",
-    alignItems: "center",
-},
-progressBar: {
-    width: 30,
-    borderRadius: 2,
-    backgroundColor: colors.primary,
-    height: 6,
-    marginRight: 6,
-},
-progressBar2: {
-    width: 22,
-    borderRadius: 2,
-    backgroundColor: colors.primary,
-    height: 6,
-    marginRight: 6,
-},
-progressBar3: {
-    width: 20,
-    borderRadius: 2,
-    backgroundColor: colors.primary,
-    height: 6,
-    marginRight: 6,
-},
-smallText: {
-    fontSize: 10,
-    color: "white"
-},
-activitySection: {
-    marginTop: 16,
-},
-activitySectionTitle: {
-    fontSize: 16,
-    fontWeight:"500",
-    color: "white",
-    paddingLeft: 18
-},
-subactivitySection: {
-    alignItems: "center",
-},
-post: {
-    marginTop: 10,
-},
-posterInfo: {
-    flexDirection: "row",
-    marginBottom: 8,
-},
-postImageContainer: {
-    width: 253, 
-    height: 175,
-    // when image is bigger than specified dimensions, it will be cutoff
-    overflow: "hidden",
-    backgroundColor: colors.primary,
-
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-},
-posterIcon: {
-    position: 'absolute',
-    left: -35,
-},
-postContentContainer: {
-    position: "relative"
-},
-logProgressButton:{
-  marginRight:"5%",
-  marginLeft:"5%",
-  paddingTop:18,
-  paddingBottom:18,
-  backgroundColor:'white',
-  borderRadius:10,
-},
-logProgressText:{
-    color:'#605F5F',
-    fontSize: 18,
-    fontWeight: "500",
-    textAlign:'center',
-    paddingLeft : 10,
-    paddingRight : 10
+cameraBar: {
+    alignItems: "center"
 }
 });
