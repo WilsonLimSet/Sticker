19f05608a6ea54684049d44e26c081d64a3e74f8
arm-out <arminsuraj@gmail.com>
2023-04-12 02:08:38 -0700

    refactor init

--- README.md
+++
@@ -1,5 +0,0 @@
-1. Git Clone
-2. CD into Sticker
-3. yarn
-4. create .env file and copy paste from email
-5. expo start 
--- colors.js
+++
@@ -1,9 +0,0 @@
-export default {
-    primary: '#B7DBEA',
-    gray: '#C5C5C7',
-    mediumGray: '#F6F7FB',
-    lightGray: '#EAEAEA',
-    darkGray: "#343434",
-    tabBar: "#E9E9E9",
-    placeholderGray: "#C1C0C0",
-};
