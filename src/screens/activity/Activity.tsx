3ea28473faf84be3c8e95501ace182fe9bf74c1a
arm-out <arminsuraj@gmail.com>
2023-04-13 00:44:52 -0700

    activity

---
+++ src/screens/activity/Activity.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+interface ActivityProps {
+
+}
+
+export const Activity: React.FC<ActivityProps> = ({}) => {
+    return (
+        <View>
+            <Text>
+                WIP: ACTIVITY SCREEN
+            </Text>
+        </View>
+    );
+}
