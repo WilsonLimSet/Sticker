5376c3e80006e6452439fb8acf1467e4793075a6
arm-out <arminsuraj@gmail.com>
2023-04-14 18:55:11 -0700

    Refactor

--- App.tsx
+++ App.tsx
@@ -1,16 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Login } from "./src/screens/login/Login";
-import { SignUp } from "./src/screens/login/SignUp";
-import { Home } from "./src/screens/home/Home";
-import React from "react";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import challengeReducer from "./src/redux/challengeSlice";
-import { Explore } from "./src/screens/explore/Explore";
-import { Activity } from "./src/screens/activity/Activity";
-import { Create } from "./src/screens/create/Create";
-import { Profile } from "./src/screens/profile/Profile";
-import { Routes } from "./src/navigation/Routes";
+import { StyleSheet } from "react-native";
 import { Providers } from "./src/navigation/Providers";
 
 export default Providers;
