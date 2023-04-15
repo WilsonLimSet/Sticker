eb590b973c72fc61d2d2b7c77e89907341f3e1dc
arm-out <arminsuraj@gmail.com>
2023-04-14 19:12:06 -0700

    remove tab header

--- src/navigation/AppTabs.tsx
+++ src/navigation/AppTabs.tsx
@@ -4,22 +4,98 @@ import { AppParamList } from "./AppParamList";
 import { Activity } from "../screens/activity/Activity";
 import { Create } from "../screens/create/Create";
 import { Explore } from "../screens/explore/Explore";
-import { Profile } from "../screens/profile/Profile";
 import { HomeStack } from "./app-nav/HomeStack";
 import { ProfileStack } from "./app-nav/ProfileStack";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { colors } from "../styles/colors";
 
 interface AppTabsProps {}
 
 const Tabs = createBottomTabNavigator<AppParamList>();
 
-export const AppTabs: React.FC<AppTabsProps> = ({}) => {
+export const AppTabs: React.FC<AppTabsProps> = ({ routeName }) => {
     return (
-        <Tabs.Navigator>
-            <Tabs.Screen name="HomeStack" component={HomeStack} />
-            <Tabs.Screen name="Explore" component={Explore} />
-            <Tabs.Screen name="Create" component={Create} />
-            <Tabs.Screen name="Activity" component={Activity} />
-            <Tabs.Screen name="ProfileStack" component={ProfileStack} />
+        <Tabs.Navigator
+            tabBarOptions={{
+                showLabel: false,
+            }}
+            barStyle={{
+                display: routeName === "Take Photo" ? "none" : "flex",
+                backgroundColor: colors.tabBar,
+                position: "absolute",
+                overflow: "hidden",
+                borderTopLeftRadius: 30,
+                borderTopRightRadius: 30,
+            }}
+            activeColor={colors.darkGray}
+            screenOptions={{ headerShown: true }}
+        >
+            <Tabs.Screen
+                name="HomeStack"
+                component={HomeStack}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="home"
+                            color={color}
+                            size={26}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Explore"
+                component={Explore}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="magnify"
+                            color={color}
+                            size={26}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Create"
+                component={Create}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="plus-circle"
+                            color={color}
+                            size={26}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Activity"
+                component={Activity}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons
+                            name="dynamic-feed"
+                            color={color}
+                            size={26}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="ProfileStack"
+                component={ProfileStack}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons
+                            name="account-circle"
+                            color={color}
+                            size={26}
+                        />
+                    ),
+                }}
+            />
         </Tabs.Navigator>
     );
 };
