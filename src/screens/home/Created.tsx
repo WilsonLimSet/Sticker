396b94f921802ff11626512b8316f0d52bdb2484
WilsonLimSet <limsetia@usc.edu>
2023-04-27 22:26:00 -0700

    profile pics showing on home screen, and fixed edit profile pic

--- src/screens/home/Created.tsx
+++ src/screens/home/Created.tsx
@@ -1,32 +1,72 @@
 import { getAuth } from 'firebase/auth';
-import React from 'react'
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import React, { useEffect, useState } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
 import { TouchableOpacity, View, Image, Text, StyleSheet } from 'react-native';
 import { colors } from '../../styles/colors';
+import { database } from "../../api/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
 
 interface CreatedProps {
-
+  id: string;
+  name: string;
+  friends: string[]; // array of friend IDs
+  handleNavigation: (id: string) => void;
 }
 
-export const Created: React.FC<CreatedProps> = ({id, name, handleNavigation}) => {
+export const Created: React.FC<CreatedProps> = ({ id, name, friends, handleNavigation }) => {
     const auth = getAuth();
-    const profileImageUrl = auth.currentUser?.photoURL;
-    // TODO: useNavigation is not a function
-	// const navigation = useNavigation();
+    const storage = getStorage();
+  
+    const [profileImageUrls, setProfileImageUrls] = useState<string[]>([]);
+  
+    useEffect(() => {
+      const fetchFriendPics = async () => {
+        try {
+          const challengeDocRef = doc(database, "userChallenges", id);
+          const challengeDoc = await getDoc(challengeDocRef);
+          if (challengeDoc.exists()) {
+            const friendsList = challengeDoc.data().friends;
+            const imageUrls: string[] = [];
+            for (const friendId of friendsList) {
+              const friendProfileRef = ref(storage, `profilepics/${friendId}`);
+              try {
+                const downloadURL = await getDownloadURL(friendProfileRef);
+                imageUrls.push(downloadURL);
+              } catch (error) {
+                console.log(`Could not load profile image for friend with ID ${friendId}:`, error);
+              }
+            }
+            setProfileImageUrls(imageUrls);
+            console.log("Friends ", friendsList);
+          } else {
+            console.log("No challenge document found");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+  
+      fetchFriendPics();
+    }, []);
+  
 
     return (
         <TouchableOpacity onPress={() => handleNavigation(id)}>
             <View style={styles.entry}>
                 <View style={styles.aboveFooter}>
-                <View style={styles.circle}></View>
-                <Image source={require('./../../../assets/star-logo-simplified.png')} />
-                </View>
-                <View style={styles.footer}>
-                <Text style={styles.title}>{name}</Text>
-                <View style={styles.profileBar}>
-                    <Image source={{ uri: profileImageUrl }} style={styles.profileBarProfiles}/>
-                    <Image source={{ uri: profileImageUrl }} style={styles.profileBarProfiles}/>
-                </View>
-                </View>
+                    <View style={styles.circle}></View>
+                        <Image source={require('./../../../assets/star-logo-simplified.png')} />
+                    </View>
+                    <View style={styles.footer}>
+                        <Text style={styles.title}>{name}</Text>
+                        <View style={styles.profileBar}>
+      {profileImageUrls.map((url, index) => (
+        <Image source={{ uri: url }} style={styles.profileBarProfiles} key={index} />
+      ))}
+    </View>
+                    </View>
             </View>
         </TouchableOpacity>
     );
